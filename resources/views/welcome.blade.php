92b3fb7dc6db949c172b8941bd7c480bb7b517b8
İbrahim MEHMET <ibrahim.mehmet1991@gmail.com>
2016-08-04 14:40:55 +0300

    json calisior

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,28 +4,50 @@
     <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
     <title>Get Json Data</title>
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
+    
 </head>
 <body>
-<div>
-    <div>
-        {{ ($JsonData) }}
-    </div>
+    <button id="post">post ile veri al</button>
 
-    {{--<script>--}}
-    {{--$(document).ready(function () {--}}
-    {{--var   // url değişkenine json dosyamızın bulunduğu yeri yazıyoruz.--}}
-    {{--$.getJSON(url, function (data)  // url  deki json getJSON komutuyla data değişkenine atıyoruzve bir fonsiyonda kullanıyoruz--}}
-    {{--{--}}
-    {{--for (var i = 0; i < data.length; i++) {  // gelen datamızın kaç elemanlı olduğunu bilmediğimiz için length komutu bize sayısını veriyor ve o kadar  döndürüyoruz--}}
-    {{--if (data[i].enstituAdi == "Fen Bilimleri Enstitüsü")  // burası alıştırmamızın fonksiyonunu arttırmak için yazdım. gelen datadan belli şartı sağlayanları  listelemek için.  benim datamda toplamda 3 değer var ama listelediğinizde şartı  sağlayan sadece 2 data dönecektir.--}}
-    {{--{--}}
-    {{--document.write(data[i].ogrenciId + " " + data[i].ogrenciAdi + " " + data[i].ogrenciSoyadi + " " + data[i].okulAdi + " " + data[i].enstituAdi + "<br>");   //burada da ekrana şartı asağlayan verileri listeliyoruz. Verileri yazdırırken, verilerimiz satır satır okunur ve bu satırdaki her biri bir değişkene atanmıştır.  değişkenleri görmek için json dosyasını açıp bakabilirsiniz.--}}
-    {{--}--}}
-    {{--}--}}
-    {{--});--}}
-    {{--});--}}
-    {{--</script>--}}
-</div>
+    <button id="get">get ile veri al</button>
+
+    <div class="veri"></div>
+
+    <script type="text/javascript">
+
+
+
+    $("#post").click(function(){
+
+        $.ajax({
+            url: "{{ action('DefaultController@index') }}",
+            type: "post",
+            data: { _token: "{{ csrf_token() }}", url: "http://localhost/test.php" },
+            success: function (data) {
+                alert(JSON.stringify(data.veri[0].tags[1]));
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        })
+
+    })
+
+
+
+    
+
+    /*    sonucAl() fonksiyonu sayesinde JSON ile verilerimizi çağırıyoruz.    */
+
+    function sonucAl()
+
+    {
+     
+
+    }
+
+ </script>
 </body>
 
 </html>
---
+++ test.php
@@ -0,0 +1,333 @@
+<?php 
+
+$json = '[
+  {
+    "_id": "57a327cf11f30f2af5b0c296",
+    "index": 0,
+    "guid": "29d7d5ee-90a4-4be0-a37d-4e8104ede2a9",
+    "isActive": true,
+    "balance": "$2,781.23",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "green",
+    "name": "Lowe Winters",
+    "gender": "male",
+    "company": "INCUBUS",
+    "email": "lowewinters@incubus.com",
+    "phone": "+1 (965) 406-3585",
+    "address": "143 Henderson Walk, Welda, American Samoa, 441",
+    "about": "Do proident sint reprehenderit ipsum et eiusmod laborum esse exercitation reprehenderit. Aliquip sint magna ipsum velit veniam excepteur quis ad cillum ea velit. Incididunt est mollit nisi enim dolore sunt do laboris. Consectetur sit Lorem veniam pariatur enim ullamco. Esse duis amet voluptate nostrud incididunt aliquip est. Est incididunt nostrud exercitation nisi.\r\n",
+    "registered": "2014-01-28T07:36:22 -02:00",
+    "latitude": 44.049401,
+    "longitude": 32.763518,
+    "tags": [
+      "non",
+      "culpa",
+      "eiusmod",
+      "elit",
+      "et",
+      "aliqua",
+      "cupidatat"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Webster Washington"
+      },
+      {
+        "id": 1,
+        "name": "Nixon Mclean"
+      },
+      {
+        "id": 2,
+        "name": "Quinn Boyd"
+      }
+    ],
+    "greeting": "Hello, Lowe Winters! You have 10 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "57a327cf686f9dcf38450124",
+    "index": 1,
+    "guid": "c7915948-6439-407b-bae8-9cf195e39262",
+    "isActive": true,
+    "balance": "$2,588.02",
+    "picture": "http://placehold.it/32x32",
+    "age": 39,
+    "eyeColor": "blue",
+    "name": "Barker Small",
+    "gender": "male",
+    "company": "EARWAX",
+    "email": "barkersmall@earwax.com",
+    "phone": "+1 (896) 407-3688",
+    "address": "610 Fillmore Avenue, Kansas, Guam, 1627",
+    "about": "Consequat cupidatat esse ad in minim nostrud incididunt eu deserunt nisi elit. Adipisicing in sunt magna laboris ullamco esse aute cupidatat ipsum quis laborum eu. Aliquip reprehenderit velit elit in ea eiusmod ea amet laborum velit.\r\n",
+    "registered": "2016-04-16T01:44:54 -03:00",
+    "latitude": 74.4435,
+    "longitude": 145.816403,
+    "tags": [
+      "labore",
+      "eiusmod",
+      "aliqua",
+      "incididunt",
+      "ad",
+      "elit",
+      "laboris"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Eleanor Wallace"
+      },
+      {
+        "id": 1,
+        "name": "Gina Weiss"
+      },
+      {
+        "id": 2,
+        "name": "Estela Solomon"
+      }
+    ],
+    "greeting": "Hello, Barker Small! You have 5 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "57a327cf9a03613e3070c521",
+    "index": 2,
+    "guid": "be5bc989-9a80-4b45-945b-74d3ac54fb1a",
+    "isActive": false,
+    "balance": "$1,693.96",
+    "picture": "http://placehold.it/32x32",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Bailey Murphy",
+    "gender": "male",
+    "company": "GEEKNET",
+    "email": "baileymurphy@geeknet.com",
+    "phone": "+1 (934) 445-2814",
+    "address": "217 Cass Place, Cawood, Alaska, 9291",
+    "about": "Qui ea sunt ut ut id amet aliquip ut exercitation elit et elit quis. Qui sint nostrud ullamco aliqua consequat consectetur amet exercitation. Est incididunt in esse qui. Nostrud dolore aute enim in consequat magna deserunt commodo mollit excepteur occaecat velit deserunt. Cupidatat anim Lorem ex reprehenderit laborum. Proident consectetur do quis tempor pariatur minim adipisicing laborum mollit velit laboris in consectetur.\r\n",
+    "registered": "2014-07-13T11:56:51 -03:00",
+    "latitude": 73.992721,
+    "longitude": -175.297069,
+    "tags": [
+      "irure",
+      "do",
+      "pariatur",
+      "amet",
+      "sint",
+      "elit",
+      "ad"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Colette Zimmerman"
+      },
+      {
+        "id": 1,
+        "name": "Tanya Doyle"
+      },
+      {
+        "id": 2,
+        "name": "Louella French"
+      }
+    ],
+    "greeting": "Hello, Bailey Murphy! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "57a327cf48d62cea3895943b",
+    "index": 3,
+    "guid": "2617fbf1-ec47-4e16-824c-4d61fda2e70b",
+    "isActive": false,
+    "balance": "$3,691.70",
+    "picture": "http://placehold.it/32x32",
+    "age": 25,
+    "eyeColor": "blue",
+    "name": "Padilla Ward",
+    "gender": "male",
+    "company": "VORATAK",
+    "email": "padillaward@voratak.com",
+    "phone": "+1 (881) 594-2447",
+    "address": "768 Holly Street, Hillsboro, New York, 8746",
+    "about": "Eu quis sint anim reprehenderit veniam id ullamco non adipisicing ullamco aliqua culpa aliqua. Excepteur tempor est Lorem velit occaecat in fugiat aliquip ea eu reprehenderit consectetur qui. Sint reprehenderit duis deserunt eiusmod cupidatat duis aliqua non. Lorem est ea incididunt ad commodo anim dolor non mollit nisi enim aliqua dolor veniam.\r\n",
+    "registered": "2015-05-14T12:56:16 -03:00",
+    "latitude": 62.909903,
+    "longitude": -1.405189,
+    "tags": [
+      "nisi",
+      "reprehenderit",
+      "incididunt",
+      "in",
+      "in",
+      "in",
+      "magna"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Tina Vasquez"
+      },
+      {
+        "id": 1,
+        "name": "Lowery Mckenzie"
+      },
+      {
+        "id": 2,
+        "name": "Brooks Page"
+      }
+    ],
+    "greeting": "Hello, Padilla Ward! You have 3 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "57a327cfa8e0108fa978873c",
+    "index": 4,
+    "guid": "a3bf6fb3-1619-4b85-b629-dc5ddd094fb6",
+    "isActive": false,
+    "balance": "$2,767.34",
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "eyeColor": "green",
+    "name": "Leach Peterson",
+    "gender": "male",
+    "company": "ZILCH",
+    "email": "leachpeterson@zilch.com",
+    "phone": "+1 (890) 528-2899",
+    "address": "432 Paerdegat Avenue, Tilden, New Mexico, 6648",
+    "about": "Nisi excepteur adipisicing excepteur fugiat magna quis ad. Ut aliquip culpa non consequat consequat. Officia cupidatat deserunt sit exercitation irure est esse dolore sit aute occaecat quis consequat.\r\n",
+    "registered": "2015-07-04T01:04:55 -03:00",
+    "latitude": -86.487742,
+    "longitude": 102.252405,
+    "tags": [
+      "aliquip",
+      "irure",
+      "veniam",
+      "cillum",
+      "ex",
+      "consequat",
+      "dolor"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Adams Ayala"
+      },
+      {
+        "id": 1,
+        "name": "Serrano Chapman"
+      },
+      {
+        "id": 2,
+        "name": "Mclaughlin Grant"
+      }
+    ],
+    "greeting": "Hello, Leach Peterson! You have 10 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "57a327cf09a608625f37e0ee",
+    "index": 5,
+    "guid": "c0606d41-a1bd-4b5b-9f92-46beef0bca4f",
+    "isActive": false,
+    "balance": "$2,215.33",
+    "picture": "http://placehold.it/32x32",
+    "age": 27,
+    "eyeColor": "brown",
+    "name": "Marta Fitzgerald",
+    "gender": "female",
+    "company": "BALUBA",
+    "email": "martafitzgerald@baluba.com",
+    "phone": "+1 (991) 539-2334",
+    "address": "966 Sedgwick Place, Valmy, Missouri, 936",
+    "about": "Magna incididunt dolore eiusmod ad. Officia pariatur nisi id qui. Anim amet tempor cillum Lorem. Ullamco sunt occaecat exercitation fugiat adipisicing labore laborum anim id elit nisi officia consectetur laboris. Eiusmod enim cupidatat magna dolor veniam pariatur duis. Incididunt nulla enim cillum sit ut duis dolore amet.\r\n",
+    "registered": "2014-01-24T08:10:15 -02:00",
+    "latitude": 40.660753,
+    "longitude": 61.871544,
+    "tags": [
+      "et",
+      "culpa",
+      "nisi",
+      "veniam",
+      "est",
+      "pariatur",
+      "nostrud"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Patsy Montoya"
+      },
+      {
+        "id": 1,
+        "name": "Tracie Acosta"
+      },
+      {
+        "id": 2,
+        "name": "Olga Velez"
+      }
+    ],
+    "greeting": "Hello, Marta Fitzgerald! You have 9 unread messages.",
+    "favoriteFruit": "strawberry"
+  },
+  {
+    "_id": "57a327cf7464540959a35470",
+    "index": 6,
+    "guid": "af4e4469-8d8a-42b2-8296-e97b7e5e4241",
+    "isActive": false,
+    "balance": "$3,648.46",
+    "picture": "http://placehold.it/32x32",
+    "age": 39,
+    "eyeColor": "blue",
+    "name": "Vincent Francis",
+    "gender": "male",
+    "company": "RODEOMAD",
+    "email": "vincentfrancis@rodeomad.com",
+    "phone": "+1 (991) 584-2238",
+    "address": "146 Lott Avenue, Esmont, South Carolina, 106",
+    "about": "Sunt consequat est ex sit sint in amet. Duis esse incididunt non exercitation ipsum. Proident do amet labore proident voluptate veniam veniam culpa nostrud. Elit proident aliquip pariatur aute aliqua dolor sint veniam.\r\n",
+    "registered": "2015-07-02T05:14:09 -03:00",
+    "latitude": -63.913039,
+    "longitude": 70.997648,
+    "tags": [
+      "commodo",
+      "irure",
+      "enim",
+      "aute",
+      "dolor",
+      "consectetur",
+      "commodo"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Crosby Blevins"
+      },
+      {
+        "id": 1,
+        "name": "Byers Morse"
+      },
+      {
+        "id": 2,
+        "name": "Hebert Medina"
+      }
+    ],
+    "greeting": "Hello, Vincent Francis! You have 2 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]';
+
+
+print_r($json);
+
+
+
+
+
+
+
+
+
+
+ ?>
